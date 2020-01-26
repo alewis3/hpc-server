40c19d02e3d1e188e1329e259567dec9822c6dc2
Amanda Lewis <alewis3@stedwards.edu>
2020-01-25 23:20:44 -0600

    Moved all apidoc notation to one file (doc.js) and updated the docs.

--- routes/users.js
+++ routes/users.js
@@ -4,62 +4,6 @@ const bcrypt = require('bcrypt');
 const is = require("is_js");
 let User = require('../models/User');
 
-/**
- * @apiDefine 201 Created 201
- * The user was created successfully.
- */
-/**
- * @api {post} /users/register Create/register a new user
- * @apiName CreateUser
- * @apiGroup Users
- * @apiDescription This route handles registering a user. The request will include an email, password, first and last name, account type, and a birthday in string format.
- *
- * @apiParam {String} email A unique email to register with (This acts as their username)
- * @apiParam {String} password A strong password in plaintext. This is hashed on the API side.
- * @apiParam {Object} name An object containing "first" and "last" fields
- * @apiParam {String} name[first] The user's first name
- * @apiParam {String} name[last] The user's last name
- * @apiParam {String="Contributor", "Homeowner", "Business Owner"} accountType The account type of the user.
- * @apiParam {Object} location An object containing "streetAddress", "city", "state", and "zip" fields
- * @apiParam {String} location[address] The user's street Address including number and street name
- * @apiParam {String} location[city] The user's city e.g. "Austin" or "Dallas"
- * @apiParam {String{2}} location[state] The user's state in this format: Texas -> "TX" or California -> "CA", etc
- * @apiParam {Number} location[zip] -> The user's zip code in number format (5 digits and valid US zip)
- *
- * @apiParamExample {json} Valid-Request-Example:
- *      {
- *          "email": "alewis3@stedwards.edu",
- *          "password": "password",
- *          "name": {
- *              "first": "Amanda",
- *              "last": "Lewis"
- *          },
- *          "accountType": "Homeowner",
- *          "location": {
- *              "address": "3001 S. Congress Ave.",
- *              "city": "Austin",
- *              "state": "TX",
- *              "zip": 78704
- *          }
- *      }
- *
- * @apiSuccess (201) {Boolean} registrationStatus Will be true if successful registration
- *
- * @apiSuccessExample CreatedResponse:
- *      HTTP/1.1 201 CREATED
- *      {
- *          "registrationStatus": true
- *      }
- *
- * @apiError FormattingError The body of the request was not formatted properly.
- *
- * @apiErrorExample BadRequest:
- *     HTTP/1.1 400 Bad Request
- *     {
- *         "registrationStatus": false,
- *         "error": "FormattingError: ______"
- *     }
- */
 router.post("/register", async (req, res) => {
 
   const json = req.body;
@@ -115,46 +59,7 @@ router.post("/register", async (req, res) => {
   } // end of else to check if valid
 });
 
-/**
- * This route handles logging in a user. This is after the user has been authenticated.
- * The validated field must be true for this user.
- *
- * @api {post} /users/login Authenticate a user for login
- * @apiName LoginUser
- * @apiGroup Users
- *
- * @apiParam {String} email The email that the user signed up with
- * @apiParam {String} password The password that the user signed up with
- *
- * @apiSuccess {Boolean} loginStatus Will be true if successful login
- * @apiSuccess {String} accountType The type of the user that was authenticated
- *
- * @apiSuccessExample Success-Response:
- *      HTTP/1.1 200 OK
- *      {
- *          "loginStatus": true,
- *          "accountType": "Contributor"
- *      }
- *
- * @apiError UserNotFound The email of the user could not be found
- *
- * @apiErrorExample UserNotFound:
- *     HTTP/1.1 404 Not Found
- *     {
- *       "loginStatus": false,
- *       "error": "UserNotFound"
- *     }
- *
- * @apiError WrongCredentials The password of the user does not match the password in the DB
- *
- * @apiErrorExample WrongCredentials:
- *     HTTP/1.1 401 Unauthorized
- *     {
- *       "loginStatus": false,
- *       "error": "WrongCredentials"
- *     }
- *
- */
+
 router.post('/login', async (req, res) => {
   const json = req.body;
   const email = json.email;
@@ -172,59 +77,12 @@ router.post('/login', async (req, res) => {
   return res.status(200).send({loginStatus: true, "accountType": user.accountType, "id": user._id});
 });
 
-/*
- * @api {patch} /users/profile Let a user edit or update their profile
- * @apiName PatchProfile
- * @apiGroup Users
- * @apiDescription This path will be called on whenever the user makes changes to their account and presses save. All fields are optional but at least one must be sent in for the request call to make sense.
- *
- * @apiParam {String} [email] A unique email to register with (This acts as their username)
- * @apiParam {String} [password] A strong password in plaintext. This is hashed on the API side.
- * @apiParam {Object} [name] An object containing "first" and "last" fields
- * @apiParam {String} [name[first]] The user's first name
- * @apiParam {String} [name[last]] The user's last name
- * @apiParam {String="Contributor", "Homeowner", "Business Owner"} [accountType] The account type of the user.
- * @apiParam {String} [dob] The user's Date of Birth in "MM/DD/YYYY" format
- * @apiParam {Object} [location] An object containing "streetAddress", "city", "state", and "zip" fields
- * @apiParam {String} [location[streetAddress]] The user's street Address including number and street name
- * @apiParam {String} [location[city]] The user's city e.g. "Austin" or "Dallas"
- * @apiParam {String{2}} [location[state]] The user's state in this format: Texas -> "TX" or California -> "CA", etc
- * @apiParam {Number} [location[zip]] -> The user's zip code in number format (5 digits and valid US zip)
- */
 
-/*
- * @api {get} /users/hosts?user_id=X Get all nearby hosts location
- * @apiName GetHosts
- * @apiGroup Users
- * @apiDescription This endpoint will eventually limit the search of users to a radius based on the user_id sent in and the search radius of that user, but for now it will grab all available hosts.
- *
- * @apiSuccess {Object[]} hosts The list of hosts to display on the map
- * @apiSuccess {String} hosts.userId The user id of the host
- * @apiSuccess {Object} hosts.location The location of the host
- * @apiSuccess {Number} hosts.location.lat The lat of the host
- * @apiSuccess {Number} hosts.location.long The long of the host
- * @apiSuccess {String} hosts.accountType The account type of the host (Homeowner or Business Owner)
- */
 // router.get('/hosts', function(req, res) {
 //     var userId = req.query.userId;
 //
 // });
 
-/**
- * @api {post} /users/resetPassword Reset a user's password
- * @apiName ResetPassword
- * @apiGroup Users
- * @apiDescription This route handles resetting a password
- *
- * @apiParam {String} id The id of the user
- * @apiParam {String} old The user's old password to match with
- * @apiParam {String} new The user's new password to save in the db
- *
- * @apiSuccess {Boolean} success Will be true if the old password matched and was able to be changed
- *
- * @apiError {Boolean} success Will be false if the old password did not match or the new pwd was not able to be changed
- * @apiError {String} error The error that caused success to be false
- */
 router.post('/resetPassword', async function (req, res) {
     var json = req.body;
     var user = await User.findById(json.id).exec();
