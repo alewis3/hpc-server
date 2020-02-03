2e70e90dc8e8944680b952ad20eed3e5ecda916a
Amanda Lewis <alewis3@stedwards.edu>
2020-02-03 13:26:41 -0600

    Actual update for latitude (69 mi -> 1 deg) and longitude (55 mi -> 1 deg) instead of just using 69 for both

--- helpers/withinRange.js
+++ helpers/withinRange.js
@@ -2,16 +2,18 @@
  * this is a helper method to calculate the distance between two points (lat/long)
  * and determine whether it falls within two radii (miles)
  * 1 degree of latitude = ~69 miles
- * params: Coordinate 1 (x1, y1) and a radius for coordinate 1 (radius1)
- *          Coordinate 2 (x2, y2) and a radius for coordinate 2 (radius2)
+ * 1 degree of longitude = ~55 miles
+ * params: Coordinate 1 (lat1, long1) and a radius for coordinate 1 in miles (radius1)
+ *          Coordinate 2 (lat2, long2) and a radius for coordinate 2 in miles(radius2)
  * calculates the distance and returns a boolean that will be true iff
- * radius1 is larger than the distance between the coordinates
- * and radius2 is larger than the distance between the coordinates
- * (meaning the coordinates are within both radii)
+ * coordinate 1 is within radius2 miles &&
+ * coordinate 2 is within radius1 miles
+ * (meaning the coordinates are within each others radii)
  */
-module.exports = function(x1, y1, radius1, x2, y2, radius2) {
-    // distance (in degrees) = sqrt[(x2 - x1)^2 + (y2 - y1)^2]
-    // to convert to miles: 69 * distance (in degrees)
-    var distance = 69 * Math.sqrt((Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2)));
+module.exports = function(lat1, long1, radius1, lat2, long2, radius2) {
+    // latitude distance in miles: Math.abs(lat2-lat1) * 69
+    // longitude distance in miles: Math.abs(long2-long1) * 55
+    // distance (in degrees) = sqrt[(lat in miles)^2 + (long in miles)^2]
+    var distance = Math.sqrt((Math.pow(Math.abs(lat2 - lat1) * 69, 2) + Math.pow(Math.abs(long2 - long1) * 55, 2)));
     return radius1 > distance && radius2 > distance;
 };
