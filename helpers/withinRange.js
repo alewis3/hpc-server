978c7c03c4b301b35d2c7d277856bc709093afcc
Amanda Lewis <alewis3@stedwards.edu>
2020-02-03 13:17:24 -0600

    Updated distance calculation to convert latitude/longitude degrees to miles

--- helpers/withinRange.js
+++ helpers/withinRange.js
@@ -1,6 +1,7 @@
 /*
- * this is a helper method to calculate the distance between two points
- * and determine whether it falls within two radii
+ * this is a helper method to calculate the distance between two points (lat/long)
+ * and determine whether it falls within two radii (miles)
+ * 1 degree of latitude = ~69 miles
  * params: Coordinate 1 (x1, y1) and a radius for coordinate 1 (radius1)
  *          Coordinate 2 (x2, y2) and a radius for coordinate 2 (radius2)
  * calculates the distance and returns a boolean that will be true iff
@@ -9,7 +10,8 @@
  * (meaning the coordinates are within both radii)
  */
 module.exports = function(x1, y1, radius1, x2, y2, radius2) {
-    // distance = sqrt[(x2 - x1)^2 + (y2 - y1)^2]
-    var distance = Math.sqrt((Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2)));
+    // distance (in degrees) = sqrt[(x2 - x1)^2 + (y2 - y1)^2]
+    // to convert to miles: 69 * distance (in degrees)
+    var distance = 69 * Math.sqrt((Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2)));
     return radius1 > distance && radius2 > distance;
 };
