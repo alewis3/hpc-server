aa680d09e98029586ecd391bc518d08e71853bbe
Amanda Lewis <alewis3@stedwards.edu>
2020-02-02 19:16:31 -0600

    Major changes to GET hosts route and added a GET hostsAll for testing purposes

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,10 @@ var router = express.Router();
 const bcrypt = require('bcrypt');
 const is = require("is_js");
 let User = require('../models/User');
+let withinRange = require('../helpers/withinRange');
+let reformatHomeowers = require('../helpers/reformatHomeowners');
+let reformatBusinessOwners = require('../helpers/reformatBusinessOwners');
+let isValidStateAbbreviation = require('../helpers/isValidStateAbbreviation');
 
 /**
  * POST register route
@@ -14,28 +18,43 @@ router.post("/register", async (req, res) => {
 
   const json = req.body;
 
+  // checking if the body contains valid input, if not, return a 400 BAD REQUEST with a FormattingError
   if (is.not.string(json.email) || is.not.email(json.email)) {
-      res.status(400).send({"registrationStatus": false, "error": "FormattingError: Email"})
+      res.status(400).send({success: false, error: "FormattingError: email"})
   }
   else if (is.not.string(json.password)) {
-      res.status(400).send({"registrationStatus": false, "error": "FormattingError: Password"})
+      res.status(400).send({success: false, "error": "FormattingError: password"})
   }
-  else if (is.not.string(json.name.first) || is.not.string(json.name.last)) {
-      res.status(400).send({"registrationStatus": false, "error": "FormattingError: Name"})
+  else if (is.not.object(json.name)) {
+      res.status(400).send({success: false, error: "FormattingError: name"})
   }
-  else if (is.not.string(json.accountType) || is.not.inArray(json.accountType, ["Contributor", "Homeowner", "Business Owner"])) {
-      res.status(400).send({"registrationStatus": false, "error": "FormattingError: Account Type"})
+  else if (is.not.string(json.name.first)) {
+      res.status(400).send({success: false, error: "FormattingError: name.first"})
   }
-  else if (is.not.object(json.location) ||
-      is.not.string(json.location.address) ||
-      is.not.string(json.location.city) ||
-      is.not.string(json.location.state) ||
-      (json.location.state.length !== 2) ||
-      is.not.number(json.location.zip) ||
-      is.not.usZipCode(json.location.zip)
-  ) {
-      res.status(400).send({"registrationStatus": false, "error": "Address not formatted properly."})
+  else if (is.not.string(json.name.last)) {
+      res.status(400).send({success: false, error: "FormattingError: name.last"})
   }
+  else if (is.not.string(json.accountType) || is.not.inArray(json.accountType, ["Contributor", "Homeowner", "Business Owner", "System Admin"])) {
+      res.status(400).send({success: false, error: "FormattingError: accountType"})
+  }
+  else if (is.not.object(json.location)) {
+      res.status(400).send({success: false, error: "FormattingError: location"})
+  }
+  else if (is.not.string(json.location.address)) {
+      res.status(400).send({success: false, error: "FormattingError: location.address"})
+  }
+  else if (is.not.string(json.location.city)) {
+      res.status(400).send({success: false, error: "FormattingError: location.city"})
+  }
+  else if (is.not.string(json.location.state) ||
+      !isValidStateAbbreviation(json.location.state.toUpperCase())) {
+      res.status(400).send({success: false, error: "FormattingError: location.state"})
+  }
+  else if (is.not.number(json.location.zip) ||
+      is.not.usZipCode(json.location.zip.toString())) {
+      res.status(400).send({success: false, error: "FormattingError: location.zip"})
+  }
+  // if no bad data was found, create a new user using the data!
   else {
       var newUser = new User({
           email: json.email,
@@ -53,13 +72,15 @@ router.post("/register", async (req, res) => {
           },
       });
 
+      // aaaaand save it! The pre-save hooks in ../models/User.js will format the email, name (first and last)
+      // and the location's address, city, and state!
       newUser.save(function (err, data) {
           if (err) {
               console.log(err);
-              res.status(400).send({"registrationStatus": false, "error": err});
+              res.status(500).send({success: false, error: err});
           } else {
               console.log(data);
-              res.status(201).send({"registrationStatus": true});
+              res.status(201).send({success: true});
           }
       });
   } // end of else to check if valid
@@ -76,17 +97,71 @@ router.post('/login', async (req, res) => {
   const email = json.email;
   const password = json.password;
   console.log(email);
-  var user = await User.findOne({ email: email }).exec();
-  if(!user) {
-    return res.status(401).send({ loginStatus: false, error: "The username does not exist" });
-  }
+  await User.findOne({ email: email }).exec(function (err, user) {
+      if (err) return res.status(500).send({success: false, error: err});
+      else {
+          if(!user) {
+              return res.status(404).send({ success: false, error: "UserNotFound" });
+          }
 
-  const passwordMatch = bcrypt.compareSync(password, user.password);
-  if(!passwordMatch) {
-    return res.status(401).send({ loginStatus: false, error: "WrongCredentials"});
-  }
-  return res.status(200).send({loginStatus: true, "accountType": user.accountType, "id": user._id});
+          const passwordMatch = bcrypt.compareSync(password, user.password);
+          if(!passwordMatch) {
+              return res.status(401).send({ success: false, error: "WrongCredentials"});
+          }
+          return res.status(200).send({success: true, "accountType": user.accountType, "id": user._id});
+      }
+  });
 }); // end of login route impl
+
+/**
+ * GET hostsAll
+ *
+ * To get all hosts for a contributor (For test use only, not for prod)
+ * See https://hpcompost.com/api/docs#api-Users-GetHostsAll for more info
+ */
+router.get('/hostsAll', async function(req, res) {
+    var userId = req.query.id;
+    var user = await User.findById(userId).exec();
+    if(!user) {
+        return res.status(404).send({ success: false, error: "IdNotFound"});
+    }
+    if (user.accountType !== "Contributor") {
+        return res.status(400).send({ success: false, error: "AccountTypeMismatch"});
+    }
+    else {
+
+        // find all business owners in range
+        let businessOwners = User.find({accountType: "Business Owner"}).select("location.lat location.long radius allowedItems prohibitedItems businessOwnerInfo name").exec(function (err, businessOwners) {
+            if (err) {
+                return res.status(500).send({success: false, error: err});
+            }
+            else {
+                // this call is reformatting the business owner objects to have keys to match the documentation
+                return reformatBusinessOwners(businessOwners);
+            }
+        });
+
+        // find all homeowners in range
+        let homeowners = User.find({accountType: "Homeowner"}).select("homeownerInfo.meetingPlace.lat homeownerInfo.meetingPlace.long radius allowedItems prohibitedItems name homeowerInfo.isListingOn").exec(function (err, homeowners) {
+            if (err) {
+                return res.status(500).send({success: false, error: err});
+            }
+            else {
+                // this call is reformatting the homeowner objects to have keys to match the documentation
+                return reformatHomeowers(homeowners);
+            }
+        });
+
+        // if both lists are empty, return a 204
+        if (businessOwners.length === 0 && homeowners.length === 0) {
+            return res.status(204).send({success: true, homeowners: [], businessOwners: []});
+        }
+        // otherwise return the lists as they are
+        else {
+            return res.status(200).send({success: true, homeowners: homeowners, businessOwners: businessOwners});
+        }
+    }
+});
 
 /**
  * GET hosts
@@ -100,8 +175,6 @@ router.get('/hosts', async function(req, res) {
     if(!user) {
         return res.status(404).send({ success: false, error: "IdNotFound"});
     }
-
-    // TODO add this error response to docs
     if (user.accountType !== "Contributor") {
         return res.status(400).send({ success: false, error: "AccountTypeMismatch"});
     }
@@ -109,31 +182,51 @@ router.get('/hosts', async function(req, res) {
         var clat = user.location.lat;
         var clong = user.location.long;
         var cradius = user.radius;
-        await User.find({accountType: {$in: ["Homeowner", "Business Owner"]}})
-            .select("location.lat location.long accountType radius")
-            .exec(function (err, hosts) {
-                if (hosts.length === 0) {
-                    // todo change the no content response to a successful response in the docs
-                    return res.status(204).send({success: true, hosts: []});
-                }
-                else {
-                    /*
-                    // this will be used once we have radii in the db
-                    var filteredHosts = hosts.filter(function(element) {
-                        var hradius = element.radius;
-                        var hlat = element.location.lat;
-                        var hlong = element.location.long;
-                        // distance = sqrt[(x2 - x1)^2 + (y2 - y1)^2]
-                        // or more specifically:
-                        // distance = sqrt[(ContributorLat - HostLat)^2 + (ContributorLong - HostLong)^2]
-                        var distance = Math.sqrt((Math.pow((clat - hlat), 2) + Math.pow((clong - hlong), 2)));
-                        return cradius > distance && hradius > distance;
-                    });
-                     */
-                    // for now just return a list of all the hosts
-                    return res.status(200).send({success: true, hosts: hosts});
-                }
+
+        // find all business owners in range
+        let businessOwners = User.find({accountType: "Business Owner"}).select("location.lat location.long radius allowedItems prohibitedItems businessOwnerInfo name").exec(function (err, businessOwners) {
+            if (err) {
+                return res.status(500).send({success: false, error: err});
+            }
+            else {
+                // filter out business owners not in the range
+                let filtered = businessOwners.filter(function(bo) {
+                    let hradius = bo.radius;
+                    let hlat = bo.location.lat;
+                    let hlong = bo.location.long;
+                    return withinRange(clat, clong, cradius, hlat, hlong, hradius);
+                });
+                // this call is reformatting the business owner objects to have keys to match the documentation
+                return reformatBusinessOwners(filtered);
+            }
         });
+
+        // find all homeowners in range
+        let homeowners = User.find({accountType: "Homeowner"}).select("homeownerInfo.meetingPlace.lat homeownerInfo.meetingPlace.long radius allowedItems prohibitedItems name homeowerInfo.isListingOn").exec(function (err, homeowners) {
+            if (err) {
+                return res.status(500).send({success: false, error: err});
+            }
+            else {
+                // filter out homeowners not in the range
+                let filtered =  homeowners.filter(function(ho) {
+                    let hradius = ho.radius;
+                    let hlat = ho.homeownerInfo.meetingPlace.lat;
+                    let hlong = ho.homeownerInfo.meetingPlace.long;
+                    return withinRange(clat, clong, cradius, hlat, hlong, hradius);
+                });
+                // this call is reformatting the homeowner objects to have keys to match the documentation
+                return reformatHomeowers(filtered);
+            }
+        });
+
+        // if both lists are empty, return a 204
+        if (businessOwners.length === 0 && homeowners.length === 0) {
+            return res.status(204).send({success: true, homeowners: [], businessOwners: []});
+        }
+        // otherwise return the lists as they are
+        else {
+            return res.status(200).send({success: true, homeowners: homeowners, businessOwners: businessOwners});
+        }
     }
 });
 
@@ -146,20 +239,27 @@ router.get('/hosts', async function(req, res) {
 router.post('/resetPassword', async function (req, res) {
     var json = req.body;
     var user = await User.findById(json.id).exec();
+
+    // check to make sure we got a user back
     if(!user) {
-        return res.status(401).send({ success: false, error: "The id does not exist" });
+        return res.status(404).send({ success: false, error: "IdNotFound" });
     }
 
+    // check to make sure the passwords match (the 'old'
+    // password that was sent in should equal user.password)
     const passwordMatch = bcrypt.compareSync(json.old, user.password);
+
+    // if the passwords do not match, send a 401 unauthorized
     if(!passwordMatch) {
-        return res.status(401).send({ success: false, error: "No password match" });
+        return res.status(401).send({ success: false, error: "NoMatch" });
     }
+    // otherwise set the user.password to the 'new' password sent in
     else {
         user.password = json.new;
         user.save(function(err) {
             if (err) {
                 console.log(err);
-                return res.status(500).send({success: false, error: "User could not be saved"});
+                return res.status(500).send({success: false, error: err});
             }
             else {
                 return res.status(200).send({success: true});
