76c23aebaff69838ff6a46009ed9216152162a7d
Amanda Lewis <alewis3@stedwards.edu>
2020-02-02 20:29:25 -0600

    Required is_js in a helper method

--- helpers/properFormat.js
+++ helpers/properFormat.js
@@ -1,3 +1,4 @@
+let is = require("is_js");
 /* properFormat will take in a string and trim it, and capitalize the first letter of every word
  * It accounts for numbers/other chars as well, and will leave numbers and all other chars as they are.
  * so can be used for an address -> properFormat('   6600 deLmOniCo DR.   ') will return '6600 Delmonico Dr'
