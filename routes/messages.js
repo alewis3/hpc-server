0926321883d6533ef3606b82959cc2ac6940c9da
Amanda Lewis <alewis3@stedwards.edu>
2020-02-21 09:51:52 -0600

    Added content to messages.js

---
+++ routes/messages.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var router = express.Router();
+const is = require("is_js");
+let User = require('../models/User');
+let Message = require('../models/Message');
+
+module.exports = router;
